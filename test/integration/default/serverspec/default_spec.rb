cb9bf12f506916e0f77bdddbb2ed0e3ed5f66eeb
Evan Sosenko <razorx@evansosenko.com>
2015-06-30 15:31:50 -0700

    Add integration tests.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
 describe 'bsdznc::default' do
-  describe command('ls -al /') do
-    its(:stdout) { should match(/bin/) }
+  describe package('znc') do
+    it { should be_installed }
+  end
+
+  describe user('znc') do
+    it { should exist }
+    it { should have_uid 194 }
+    it { should have_home_directory '/nonexistent' }
+    it { should have_login_shell '/usr/sbin/nologin' }
+  end
+
+  describe group('znc') do
+    it { should exist }
+    it { should have_gid 194 }
+  end
+
+  describe service('znc') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port(194) do
+    it { should be_listening }
+  end
+
+  describe port(443) do
+    it { should be_listening }
   end
 end
