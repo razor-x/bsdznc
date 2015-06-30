407d1976060480e6268fa4408a8ee76ea4186716
Evan Sosenko <razorx@evansosenko.com>
2015-06-30 11:56:50 -0700

    Refactor and add matchers.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,9 +1,21 @@
 if defined?(ChefSpec)
-  def configure_znc(name)
+  def install_znc(name)
     ChefSpec::Matchers::ResourceMatcher.new(:bsdznc, :create, name)
   end
 
   def start_znc(name)
     ChefSpec::Matchers::ResourceMatcher.new(:bsdznc, :start, name)
+  end
+
+  def stop_znc(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:bsdznc, :stop, name)
+  end
+
+  def restart_znc(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:bsdznc, :restart, name)
+  end
+
+  def remove_znc(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:bsdznc, :destroy, name)
   end
 end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'bsdznc::default' do
   let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
 
-  it 'creates the znc server' do
-    expect(chef_run).to configure_znc('default')
+  it 'installs the znc server' do
+    expect(chef_run).to install_znc('default')
   end
 
   it 'creates the ssl certificate' do
